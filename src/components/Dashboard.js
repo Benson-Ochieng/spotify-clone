09ed7f9bb49d756a03342f3cfd06104b2b2ffa98
Benson-Ochieng <markbenson10368@gail.com>
2023-01-23 09:49:04 +0300

    M: Create spotify clone sidebar

--- src/components/Dashboard.js
+++ src/components/Dashboard.js
@@ -1,14 +1,26 @@
-import React, { Component } from 'react'
+import React, { Component } from "react";
+import Content from "./Content";
+import NavBar from "./NavBar";
 
 // import components
-import SideBar from './SideBar';
+import SideBar from "./SideBar";
 
 export default class Dashboard extends Component {
   render() {
     return (
-      <div className='d-flex justify-content-center'>
-        <SideBar />
+      <div className="container-fluid">
+        <div className="row">
+          <div className="col-md-4">
+            <SideBar />
+          </div>
+          <div className="col-md-8">
+            <NavBar />
+            <div className="content">
+              <Content />
+            </div>
+          </div>
+        </div>
       </div>
-    )
+    );
   }
 }
--- src/components/SideBar.js
+++ src/components/SideBar.js
@@ -1,18 +1,54 @@
-import React, { Component } from 'react'
+import React, { Component } from "react";
+import "../styles/SideBar.css";
+
+import { BiSearch } from "react-icons/bi";
+import { MdHomeFilled } from "react-icons/md";
+import { VscLibrary } from "react-icons/vsc";
+import { MdPlaylistAdd } from "react-icons/md";
+import { FcLike } from "react-icons/fc";
 
 // Import Components
-import NavBar from './NavBar'
+import NavBar from "./NavBar";
 
 export default class SideBar extends Component {
   render() {
     return (
-        <>
-              <div className='col-md-4'>
-                    <h1>SideBar</h1>
-                </div>
-                <NavBar />
-        </>
+      <>
+        <div className="col-md-4">
+          <div className="side_bar">
+            <h1 className="dotted_menu">...</h1>
+            <div className="sideBar">
+              <ul className="side_menus">
+                <a href="#" className="nav-item">
+                  <MdHomeFilled className="home-icon" />
+                  <li className="home">Home</li>
+                </a>
 
-    )
+                <a href="#" className="nav-item">
+                  <BiSearch className="search_icon" />
+                  <li className="search">Search</li>
+                </a>
+
+                <a href="#" className="nav-item">
+                  <VscLibrary className="library-icon" />
+                  <li className="search">Your Library</li>
+                </a>
+
+                <a href="#" className="nav-item">
+                  <MdPlaylistAdd className="create-icon" />
+                  <li className="search">Create Playlist</li>
+                </a>
+
+                <a href="#" className="nav-item">
+                  <FcLike className="liked-icon" />
+                  <li className="search">Liked Songs</li>
+                </a>
+              </ul>
+            </div>
+            <hr className="line" />
+          </div>
+        </div>
+      </>
+    );
   }
 }
