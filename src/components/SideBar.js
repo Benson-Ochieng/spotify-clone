d3d9e10a6716178622b1cb1c3bbc9e2eedbb7cda
Benson-Ochieng <markbenson10368@gail.com>
2023-01-23 10:10:50 +0300

    M: li inside a tag bug fix

--- src/components/SideBar.js
+++ src/components/SideBar.js
@@ -14,35 +14,48 @@ export default class SideBar extends Component {
   render() {
     return (
       <>
-        <div className="col-md-4">
+        <div className="col-md-6">
           <div className="side_bar">
-            <h1 className="dotted_menu">...</h1>
+            <a href="#" className="dotted-menu">
+              <h1 className="dotted_menu">...</h1>
+            </a>
+
             <div className="sideBar">
               <ul className="side_menus">
-                <a href="#" className="nav-item">
-                  <MdHomeFilled className="home-icon" />
-                  <li className="home">Home</li>
-                </a>
+                <li>
+                  <a href="#" className="nav-item">
+                    <MdHomeFilled className="home-icon" />
+                    Home
+                  </a>
+                </li>
 
-                <a href="#" className="nav-item">
-                  <BiSearch className="search_icon" />
-                  <li className="search">Search</li>
-                </a>
+                <li>
+                  <a href="#" className="nav-item">
+                    <BiSearch className="search_icon" />
+                    Search
+                  </a>
+                </li>
 
-                <a href="#" className="nav-item">
-                  <VscLibrary className="library-icon" />
-                  <li className="search">Your Library</li>
-                </a>
+                <li>
+                  <a href="#" className="nav-item">
+                    <VscLibrary className="library-icon" />
+                    Your Library
+                  </a>
+                </li>
 
-                <a href="#" className="nav-item">
-                  <MdPlaylistAdd className="create-icon" />
-                  <li className="search">Create Playlist</li>
-                </a>
+                <li>
+                  <a href="#" className="nav-item">
+                    <MdPlaylistAdd className="create-icon" />
+                    Create Playlist
+                  </a>
+                </li>
 
-                <a href="#" className="nav-item">
-                  <FcLike className="liked-icon" />
-                  <li className="search">Liked Songs</li>
-                </a>
+                <li>
+                  <a href="#" className="nav-item">
+                    <FcLike className="liked-icon" />
+                    Liked Songs
+                  </a>
+                </li>
               </ul>
             </div>
             <hr className="line" />
