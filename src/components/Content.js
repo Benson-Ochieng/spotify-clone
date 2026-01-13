8a29fa7dd52aeba9491b670a16dc132b89a25d93
Benson (Mark) Ochieng <markbenson10368@gmail.com>
2026-01-13 16:07:16 +0300

    Test Commit

--- src/components/Content.js
+++ src/components/Content.js
@@ -1,7 +1,7 @@
 import React, { Component } from "react";
 
 export default class Content extends Component {
-  render() {
-    return <div>Content</div>;
-  }
+    render() {
+        return <div>Ni Ku attack na kumaliza</div>;
+    }
 }
